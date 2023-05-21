cc84f35b6005c8110d5bc27ce1f89ed49773711a
Mehdi Bagheri <mehdibaghery@gmail.com>
2023-05-21 10:40:06 +0200

    change topic and requestbody

--- src/main/java/model/MessageDto.java
+++ src/main/java/model/MessageDto.java
@@ -1,10 +1,16 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class MessageDto {
-  private String message;
+
+  @JsonProperty("gpio_pin")
+  private int gpioPin;
+
+  private boolean activated;
 }
